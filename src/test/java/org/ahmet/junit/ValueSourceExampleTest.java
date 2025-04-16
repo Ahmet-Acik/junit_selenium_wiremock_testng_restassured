8c04fa41bb19b2bcc80270ea1bc11c0e7f4950a2
drahmetacik <a.acik@icloud.com>
2025-04-16 23:20:57 +0100

    add ValueSourceExampleTest for parameterized testing with JUnit

---
+++ src/test/java/org/ahmet/junit/ValueSourceExampleTest.java
@@ -0,0 +1,15 @@
+package org.ahmet.junit;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ValueSourceExampleTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void testWithValueSource(int number) {
+        assertTrue(number > 0 && number < 6);
+    }
+}
