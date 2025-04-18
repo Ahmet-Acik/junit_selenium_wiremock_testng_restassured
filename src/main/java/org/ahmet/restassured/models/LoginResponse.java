ee5efbc250a1c60538b262ad2470359680c1e995
drahmetacik <a.acik@icloud.com>
2025-04-18 15:45:38 +0100

    add model classes for Login and LoginResponse

---
+++ src/main/java/org/ahmet/restassured/models/LoginResponse.java
@@ -0,0 +1,14 @@
+package org.ahmet.restassured.models;
+
+public class LoginResponse {
+    private String token;
+
+    // Getters and Setters
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
